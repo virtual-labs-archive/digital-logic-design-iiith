3d37655e989ddb5806e1f5dbce7d8c27728f6c87
krishnakumar.singh <krishnakumar.singh@a9956e04-5120-42bd-bbb4-6ec015d60dce>
2012-02-13 06:40:52 +0000

    git-svn-id: file:///labs/cse15/svn/repo_dld@55 a9956e04-5120-42bd-bbb4-6ec015d60dce

--- src/lab/exp2/exp2.php
+++ src/lab/exp2/exp2.php
@@ -11,6 +11,7 @@ if(y<660)
 	y=660;
 }
 document.getElementById("applet_embed").innerHTML='<applet code="dldvirtuallabs.simulationApplet" archive="../DLDVirtualLabs.jar"'+' HEIGHT='+y+' WIDTH='+x+'>' +'<param name="file_name" value="line.txt"><param name="content1" value="Make 8 X 1 multiplexer with the help of 2 X 1 multiplexer"><param name="file_list" value="Multiplexer,8_1_multiplexer"></applet>' ;
+alert("yeah");
 </script>
 <div id="applet_embed"></div> 
 </body>
