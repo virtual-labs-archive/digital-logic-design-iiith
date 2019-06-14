89ce64c60ff775c9ae1fbb55e00150f35cd19672
vinaytejab <vinaytejaemailid@gmail.com>
2019-06-14 07:57:09 +0530

    changes

--- SRIP/Codes/main.js
+++ SRIP/Codes/main.js
@@ -2,6 +2,7 @@
 $("#check1").click(function(event) {
   /*sessionStorage.removeItem('8bitRingCounter');
   sessionStorage.removeItem('8bitCounter');*/
+  sessionStorage.clear();
   location.reload();
 });
 /*  Go JS ..junction later on changed the library
