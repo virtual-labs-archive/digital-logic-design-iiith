ab194f022e780cd603bb9d928411253780fcc76e
Pratyush Narain <32239181+Pratyush1197@users.noreply.github.com>
2019-08-05 19:30:41 +0530

    Update main.js

--- SRIP_2/codes/main.js
+++ SRIP_2/codes/main.js
@@ -5,3 +5,9 @@ const port = 3000
 var router = express.Router();
 var path = require('path');
 app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    res.render(__dirname + "/index");
+    app.use(express.static(__dirname));
+    app.use(express.static(__dirname));
+})
